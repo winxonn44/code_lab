b64bf720ac9e72a95ad75539f1e12ab73035252f
winxonn44 <winxonnguyen44@gmail.com>
2025-06-17 00:25:25 -0700

    Day 5 lol

---
+++ code_lab_00/week_1/Day5.js
@@ -0,0 +1,29 @@
+let favFoods = ["sushi", "steak", "ice cream", "ramen", "idk"];
+
+favFoods.push("boba tea");
+favFoods.shift(0);
+
+let book00 = {
+    title: "Oliver Twist",
+    author: "man idek",
+    year: "lol idk"
+}
+
+let bookLibrary = {
+    book01: {
+        title: "Demise",
+        author: "Regal John",
+        year: 1984
+    },
+    book02: {
+        title: "Legend",
+        author: "Smith John",
+        year: 2000
+    },
+    book03: {
+        title: "Calamity",
+        author: "Brandon John",
+        year: 76
+    }
+};
+console.log(bookLibrary.book02.title);
